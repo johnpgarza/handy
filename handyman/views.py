ec63cbc9b7ea8891d0a824f68797d7601ff6ff35
johnpgarza <jgarza@unomaha.edu>
2020-03-24 20:44:47 -0500

    Added User Registration

--- handyman/views.py
+++ handyman/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -24,3 +24,17 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, '../templates/registration/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, '../templates/registration/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, '../templates/registration/register.html', {'user_form': user_form})
