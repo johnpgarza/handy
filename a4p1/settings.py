53ec41eba6e56498135589a8651b387e873fcc85
johnpgarza <jgarza@unomaha.edu>
2020-03-29 20:12:12 -0500

    set static root

--- a4p1/settings.py
+++ a4p1/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = "/a4p1/static/"
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, "static"),
     '/a4p1/static/',
